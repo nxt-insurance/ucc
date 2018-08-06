41b4d09156cee5c5293bd28e7798b24a4ed1c65f
Marlena Kropidlowska <marlena@Marlenas-MacBook-Pro.local>
2018-08-06 12:11:45 +0200

    Remove questions feature

--- src/App.js
+++ src/App.js
@@ -16,17 +16,7 @@ const QuestionsToType = (props) => {
                 value={value}
                 placeholder="Write your question here"
             />
-            <button onClick={(event) => this.props.handleAddClick(index, event.target.value)}>Add</button>
-        </div>
-    )
-}
-
-function AnswersToType(props) {
-    return (
-        <div key={`answer-${props.index}`}>
-            <label htmlFor={`answer-${props.index}`}>Answer text</label>
-            <input type="txt" id={`answer-${props.index}`} placeholder="Write your answers here"/>
-            <button>Add</button>
+            <button onClick={(event) => props.handleRemoveClick(index, event.target)}>Remove</button>
         </div>
     )
 }
@@ -40,34 +30,22 @@ function QuestionsToShow(props) {
     )
 }
 
-function AnswersToShow(props) {
-    return (
-        <div key={`answersToShow-${props.index}`}>
-            <div>{props.item}</div>
-        </div>
-    )
-}
+
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            questions: ['question 1', 'question 2'],
-            answers: []
+            questions: []
         }
     }
 
     handleClickQuestion = (text) => {
         this.setState({
-            questions: this.state.questions.concat(text)
+            questions: this.state.questions.concat("question " + this.state.questions.length)
         });
     };
 
-    handleClickAnswer = (event) => {
-        this.setState({
-            answers: this.state.answers.concat('Give your answer here')
-        });
-    };
 
     handleInputChange = (index, newText) => {
         this.setState({
@@ -81,8 +59,13 @@ class App extends Component {
         })
     }
 
-    handleAddClick = (event) => {
-        console.log(this.state.value);
+    handleRemoveClick = (index) => {
+        this.setState({
+            questions: this.state.questions.filter((el, i) => {
+                const includeElement = (i !== index)
+                return includeElement
+            })
+        })
     }
 
     render() {
@@ -93,7 +76,6 @@ class App extends Component {
                 <div className="write-section">
                     <div>
                         <button onClick={this.handleClickQuestion}>Add question</button>
-                        <button onClick={this.handleClickAnswer}>Add answer</button>
                     </div>
                     <div>
                         <div>{this.state.questions.map((value, index) => <QuestionsToType
@@ -101,13 +83,16 @@ class App extends Component {
                             value={value}
                             key={"asdef" + index}
                             handleInputChange={this.handleInputChange}
+                            handleRemoveClick={this.handleRemoveClick}
                         />)}</div>
-                        <div>{this.state.answers.map((item, index) => <AnswersToType index={index} item={item} key={"asdhf" + index}/>)}</div>
                     </div>
                 </div>
                 <div className="see-section">
-                    <div>{this.state.questions.map((item, index) => <QuestionsToShow key={"adsdf" + index} index={index} item={item}/>)}</div>
-                    <div>{this.state.answers.map((item, index) => <AnswersToShow key={"asadf" + index} index={index} item={item}/>)}</div>
+                    <div>{this.state.questions.map((item, index) => <QuestionsToShow
+                        key={"adsdf" + index}
+                        index={index}
+                        item={item}
+                    />)}</div>
                 </div>
             </div>
         );
