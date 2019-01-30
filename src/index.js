03f054a94afce9b8b2c6d0c379d173074785c8d5
Marlena Kropidlowska <m.k.kropidlowska@gmail.com>
2019-01-30 13:37:02 +0100

    Comment the cos that is breaking the app

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import store from './store'
 import Provider from 'react-redux/es/components/Provider'
-import { loadQuestions } from './actionCreators'
+// import { loadQuestions } from './actionCreators'
 
 ReactDOM.render(
   <Provider store={store}>
@@ -14,10 +14,10 @@ ReactDOM.render(
   document.getElementById('root')
 )
 
-fetch('/polish')
-  .then(res => res.json())
-  .then(result => {
-    store.dispatch(loadQuestions(result))
-  })
+// fetch('/polish')
+//   .then(res => res.json())
+//   .then(result => {
+//     store.dispatch(loadQuestions(result))
+//   })
 
 registerServiceWorker()
